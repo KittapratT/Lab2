d223932cb5e7d5fd558c8049438463857010c4ee
KittapratT <kittaprat.k@ku.th>
2025-07-03 18:03:16 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -1,19 +1,56 @@
 package lib;
 
 public class PasswordValidator {
-
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param password ที่ต้องการตรวจสอบ
+     * @return ระดับตวามปลอดภัยของรหัส
      */
-    // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
+
+      boolean UpperCase = false ;
+      boolean LowerCase = false ;
+      boolean Special = false ;
+      boolean Digit = false ;
+
+      //วนลูปเช็ครหัสผ่านมี ตัวพิมพ์เล็ก,ตัวเลข,ตัวพิมพ์ใหญ่,ตัวพิเศษ ไหม
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isUpperCase(c)) {
+                UpperCase = true; 
+            }
+            else if (Character.isLowerCase(c)) {
+                LowerCase = true; 
+            }
+            else if (Character.isDigit(c)) {
+                Digit = true; 
+            }else Special = true;
+        }
+
+        //ตรวจว่าเกิน 8 ตัวอักษรหรือเป็นช่องว่างไหม
+         int minLength = 8; // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
+        if (password.length() < minLength || password == null) {
+            return PasswordStrength.INVALID;
+            }
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        //ดูว่าผ่านไปแล้วกี่อย่าง
+        int passed = 0;
+        if (password.length() >= minLength || password == null) {passed++; }
+        if (UpperCase) {passed++; }
+        if (LowerCase) {passed++; }
+        if (Digit) {passed++; }
+        if (Special) {passed++; }
+
+        //กำหนดว่าต้องผ่านกี่อย่างถึงได้ความปลอดภัยเท่าไหร่
+        if (passed >= 4) {
+            return PasswordStrength.STRONG;
+        }else if (passed >= 3) {
+            return PasswordStrength.MEDIUM;
+        }else if (passed >= 2) {
+            return PasswordStrength.WEAK;
+        }
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        return PasswordStrength.INVALID  ; 
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -13,6 +13,33 @@ public class PasswordValidatorTest{
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
         }
 
+        // Test Case 2 : รหัสผ่านมีตัวพิมพ์เล็ก,ตัวเลข,ตัวพิมพ์ใหญ่,ตัวพิเศษ อย่างใดอย่างหนึ่งจะ Weak
+        PasswordStrength result2 = PasswordValidator.validate("asdasdasd");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: Short password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected INVALID but got " + result2);
+    
+        }
+
+        // Test Case 3 : รหัสผ่านมีตัวพิมพ์เล็ก,ตัวเลข,ตัวพิมพ์ใหญ่,ตัวพิเศษ 2 อย่างจะ Medium
+        PasswordStrength result3 = PasswordValidator.validate("aAdaasasd");
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: Short password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected INVALID but got " + result3);
+    
+        }
+
+        // Test Case 4 : รหัสผ่านมีตัวพิมพ์เล็ก,ตัวเลข,ตัวพิมพ์ใหญ่,ตัวพิเศษ มากกว่า 3 อย่างจะ Strong
+        PasswordStrength result4 = PasswordValidator.validate("YTasdaasd23#");
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed: Short password is STRONG.");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected INVALID but got " + result4);
+    
+        }
+
         // --- เขียน Test Case อื่นๆ ต่อ ---
 
         System.out.println("--------------------------------");
